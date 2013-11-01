9b8c69e553c441604d883cc7c5fc93cbebce7147
cesine <cesine@yahoo.com>
2013-10-31 20:19:10 -0400

    pulled text out of the xhtml pages

--- ui/read-along-debug-view.reel/read-along-debug-view.js
+++ ui/read-along-debug-view.reel/read-along-debug-view.js
@@ -1,4 +1,5 @@
-var PageView = require("ui/page-view.reel").PageView;
+var PageView = require("ui/page-view.reel").PageView,
+    Template = require("montage/core/template").Template;
 
 exports.ReadAlongDebugView = PageView.specialize({
 
@@ -6,6 +7,33 @@ exports.ReadAlongDebugView = PageView.specialize({
         value: function() {
             console.log("Drawing ", this.item);
             this.super();
+            var pageFrame = this.element.getElementsByTagName("iFrame")[0];
+            if (pageFrame && pageFrame.src && pageFrame.src.indexOf("about:blank") === -1) {
+                var srcUri = pageFrame.src,
+                    self = this;
+
+                srcUri = srcUri.replace("http://client/index.html?file=", "");
+                console.log("Opening srcUri " + srcUri);
+                
+                require.read(srcUri).then(function(pagehtml) {
+                    var doc,
+                        template,
+                        textNode,
+                        textLines = [];
+
+                    template = Template.create();
+                    doc = template.createHtmlDocumentWithHtml(pagehtml);
+                    var textNodes = doc.getElementsByTagName("text");
+                    console.log(pagehtml, textNodes);
+
+                    for (var node = 0; node < textNodes.length; node++) {
+                        textNode = textNodes[node];
+                        textLines.push(textNode.textContent);
+                    }
+                    self.item.text = textLines.join(" ::: ");
+                });
+            }
+
         }
     },
 
