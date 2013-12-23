4f0aae7fdf476dc824cd74717124629e93a01896
cesine <cesine@yahoo.com>
2013-12-23 10:28:34 -0500

    Revert "Revert "made it so that alignment doesnt produce errors as a backend plugin""
    This reverts commit 7504befcd81de789559e48dd00a3523c1c5d1119.

--- backend_plugins/audio-alignment.js
+++ backend_plugins/audio-alignment.js
@@ -6,8 +6,17 @@ var Q = require("q"),
 var IS_IN_LUMIERES = (typeof lumieres !== "undefined");
 
 
-exports.AudioAlignment = Object.create({
+var AudioAlignment = function() {
+    console.log("Constructing AudioAlignment... ");
+    Object.call(this);
+};
 
+AudioAlignment.prototype = Object.create(Object.prototype, {
+
+    constructor: {
+        value: AudioAlignment
+    },
+    
     _backend: {
         value: null
     },
@@ -22,7 +31,7 @@ exports.AudioAlignment = Object.create({
 
     backend: {
         get: function() {
-            var self = this;`
+            var self = this;
 
             if (self._backend == null) {
                 var connection = AdaptConnection(new WebSocket("ws://localhost:" + lumieres.nodePort));
@@ -214,3 +223,6 @@ exports.AudioAlignment = Object.create({
         }
     }
 });
+
+
+exports.AudioAlignment = AudioAlignment;
