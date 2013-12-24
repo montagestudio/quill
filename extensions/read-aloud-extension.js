0ba63ea80c3b9817e9a74ac5e89deaf19cb3d510
cesine <cesine@yahoo.com>
2013-12-24 08:02:15 -0500

    running the aligner in batch works on penguins but stops
    on page three of skeleton cat

--- extensions/read-aloud-extension.js
+++ extensions/read-aloud-extension.js
@@ -1,7 +1,9 @@
 /*jshint camelcase:false, maxcomplexity:16 */ // TODO: fix these warnings
 var Montage = require("montage/core/core").Montage,
     Promise = require("montage/core/promise").Promise,
-    ImportExtension = require("core/ImportExtension").ImportExtension;
+    ImportExtension = require("core/ImportExtension").ImportExtension,
+    IMPORT_STATES = require("core/importStates").importStates,
+    ReadAlong = require("core/read-along").ReadAlong;
 
 exports.ReadAloudExtension = Montage.create(ImportExtension, {
 
@@ -18,11 +20,24 @@ exports.ReadAloudExtension = Montage.create(ImportExtension, {
                 self = this;
 
             console.log("*** customizePages with Read Aloud");
+
+            //HACK to change the progress bar... otherwise it looks like it hangs
+            var ipc = backend.get("ipc");
+            item.currentPage = 1;
+            item.status = IMPORT_STATES.generatingAudioAlignment;
+            ipc.invoke("namedProcesses", "monitor").then(function(processID) {
+                if (processID) {
+                    return ipc.invoke("send", item.processID, processID[0], ["itemUpdate", item]);
+                }
+            }, function(e) {
+                console.log("ERROR UPDATING IMPORT STATE:", e.message, e.stack);
+            });
+
             Promise.nextTick(function() {
                 self._addReadAloudToEPub(backend, item).then(function() {
                     console.log("Customized ePub with Read Aloud when applicable.");
                     deferred.resolve(item.id);
-                }).done(function(error) {
+                }, function(error) {
                     console.log("Was not able to run the read aloud processes");
                     deferred.resolve(item.id);
                 });
@@ -62,14 +77,14 @@ exports.ReadAloudExtension = Montage.create(ImportExtension, {
 
             Promise.nextTick(function() {
                 for (var pageIndex = 1; pageIndex < item.nbrPages; pageIndex++) {
-                    promises.push(self._addReadAloudToPage(backend, pageIndex));
+                    promises.push(self._addReadAloudToPage(backend, item, pageIndex));
                 }
                 Promise.allSettled(promises).then(function(result) {
                     console.log("Success of all pages when batch running the read aloud.", result);
                 }, function(error) {
                     console.log("Error on some pages when batch running the read aloud.", error);
-                }).done(function(result) {
-                    console.log("Done batch running the read aloud.", result);
+                }).done(function() {
+                    console.log("Done batch running the read aloud.");
                     deferred.resolve(item.id);
                 });
             });
@@ -78,19 +93,61 @@ exports.ReadAloudExtension = Montage.create(ImportExtension, {
     },
 
     _addReadAloudToPage: {
-        value: function(backend, pageNumber) {
+        value: function(backend, item, pageNumber) {
             var deferred = Promise.defer();
 
             Promise.nextTick(function() {
-                console.log("*** Extracting read aloud for page " + pageNumber);
+                console.log("*** Requesting read aloud for page " + pageNumber);
 
-                backend.get("read-aloud").invoke("runAligner", {}).then(function(results) {
-                    console.log("Results of calling runAligner", results);
-                    deferred.resolve("All done with page " + pageNumber);
+                var pageURL = item.destination + "/OEBPS/pages/" + pageNumber + ".xhtml",
+                    smilUrl = item.destination + "/OEBPS/overlay/" + pageNumber + ".smil",
+                    jsonUrl = item.destination + "/json/" + pageNumber + ".json",
+                    readAlong = new ReadAlong();
+
+                readAlong.readingOrder.loadFromXHTML(pageURL).then(function(order) {
+                    if (!order || order.length <= 0) {
+                        deferred.resolve("No reading order in " + pageNumber);
+                        return;
+                    }
+
+                    console.log("Recieved non-empty reading order for " + pageURL);
+                    readAlong.xhtmlUrl = pageURL;
+
+                    readAlong.triggerAlignerWithReadingOrder().then(function(results) {
+                        console.log("\tResults of calling runAligner on page " + pageNumber, results);
+                        //TODO save smil here
+                        if (results) {
+                            console.log("ready to save smil ");
+                            // console.log("ready to save smil " + JSON.stringify(results, null, 2));
+                            deferred.resolve("got " + results.length + " results on page " + pageNumber);
+                        } else {
+                            deferred.resolve("got no results on page " + pageNumber);
+                        }
+                    }, function(error) {
+                        console.log("Error calling runAligner", error);
+                    }).done(function() {
+                        var ipc = backend.get("ipc");
+
+                        //HACK to change the progress bar... otherwise it looks like it hangs
+                        item.currentPage = pageNumber + 1;
+                        item.status = IMPORT_STATES.generatingAudioAlignment;
+                        ipc.invoke("namedProcesses", "monitor").then(function(processID) {
+                            console.log("Sending generatingAudioAlignment update.");
+                            if (processID) {
+                                return ipc.invoke("send", item.processID, processID[0], ["itemUpdate", item]);
+                            }
+                        }, function(e) {
+                            console.log("ERROR UPDATING IMPORT STATE:", e.message, e.stack);
+                        }).done(function() {
+                            deferred.resolve("All done with page " + pageNumber);
+                        });
+                    });
+
                 }, function(error) {
-                    console.log("Error calling runAligner", error);
-                    deferred.reject(error);
+                    console.log("Error getting reading order.", error);
+                    deferred.resolve("Error getting reading order in page " + pageNumber);
                 });
+
 
             });
             return deferred.promise;
